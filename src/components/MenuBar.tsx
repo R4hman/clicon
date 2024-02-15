abecc92b430b2bf5e36c49c2c30aa54182ec0784
R4hman <rehmanmysayev@gmail.com>
2024-02-15 23:19:06 +0400

    getting category from api

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -20,9 +20,11 @@ import ShopNowBtn from "./reusable/ShopNowBtn";
 
 const MenuBar: React.FC = () => {
   const [clicked, setClicked] = useState<boolean>(false);
-  const { data, isLoading } = useCategories("http://localhost:3000/categories");
-  // const { data, isLoading } = useCategories("https://clicon.onrender.com/api/v1/categories");
-
+  // const { data, isLoading } = useCategories("http://localhost:3000/categories");
+  const { data: cat, isLoading } = useCategories(
+    "https://clicon.onrender.com/api/v1/categories"
+  );
+  console.log("cat", cat);
   return (
     <Menubar>
       <MenubarMenu>
@@ -38,58 +40,59 @@ const MenuBar: React.FC = () => {
         <MenubarContent className="bg-white">
           {isLoading && <div>loading...</div>}
           {!isLoading &&
-            data?.map((item: TData) => {
-              if (item.subcategories) {
+            cat?.categories?.map((item: TData) => {
+              if (item.brands) {
                 return (
                   <MenubarSub key={uuidv4()}>
                     <MenubarSubTrigger className="hover:bg-gray100 cursor-pointer">
                       <Link to={item.url}>{item.name}</Link>
                     </MenubarSubTrigger>
                     <MenubarSubContent className="-ml-56 sm:ml-0 md:ml-6 w-[468px] lg:w-[868px] bg-white p-5 flex flex-row flex-wrap lg:flex-nowrap gap-x-2 mt-[200px] lg:-mt-[68px]">
-                      <div className="basis-1/8">
-                        {item.subcategories.map((cat) => (
-                          <MenubarItem
-                            className="hover:bg-gray100 w-[100px] cursor-pointer "
-                            key={uuidv4()}
-                          >
-                            <Link to={`${item.url}/${cat}`}>{cat}</Link>
-                          </MenubarItem>
-                        ))}
-                      </div>
-                      <div className="lg:basis-1/2 basis-1/3 ">
-                        <h3 className="text-sm font-semibold mb-4">
-                          FEATURED PHONES
-                        </h3>
-                        <div className="flex flex-col gap-y-4 w-[300px]  ">
-                          {item.products
-                            ?.slice(0, 3)
-                            ?.map((product: TProduct) => (
-                              <SmallProductCard
-                                key={uuidv4()}
-                                product={product}
-                              />
-                            ))}
-                        </div>
-                      </div>
-                      <div className="w-full bg-warning200 rounded-sm p-8 flex items-center flex-col">
-                        <img src={item.mainDiscount?.img} alt="" />
-                        <h4 className="text-gray900 text-[28px] font-semibold">
-                          {item.mainDiscount?.title}
-                        </h4>
-                        <p className="text-gray700 font-normal text-[16px] leading-4 text-center mb-4">
-                          {item.mainDiscount?.subtitle}
-                        </p>
-                        <span className="mb-6 text-sm text-gray700 flex items-center gap-x-4">
-                          Starting Price:
-                          <span className="bg-white px-3 py-1.5 text-[16px] font-semibold leading-6 text-gray900  ">
-                            ${item.mainDiscount?.price} USD
-                          </span>
-                        </span>
+                      {item.brands.map((brand) => (
+                        <div key={uuidv4()}>
+                          <div className="basis-1/8">
+                            <MenubarItem className="hover:bg-gray100 w-[100px] cursor-pointer ">
+                              <Link to={`${item.name}/${brand.name}`}>
+                                {brand.name}
+                              </Link>
+                            </MenubarItem>
+                          </div>
+                          <div className="lg:basis-1/2 basis-1/3 ">
+                            <h3 className="text-sm font-semibold mb-4">
+                              FEATURED PHONES
+                            </h3>
+                            <div className="flex flex-col gap-y-4 w-[300px]  ">
+                              {brand.products
+                                ?.slice(0, 3)
+                                ?.map((product: TProduct) => (
+                                  <SmallProductCard
+                                    key={product._id}
+                                    productId={product}
+                                  />
+                                ))}
+                            </div>
+                          </div>
+                          {/* <div className="w-full bg-warning200 rounded-sm p-8 flex items-center flex-col">
+                       <img src={item.mainDiscount?.img} alt="" />
+                       <h4 className="text-gray900 text-[28px] font-semibold">
+                         {item.mainDiscount?.title}
+                       </h4>
+                       <p className="text-gray700 font-normal text-[16px] leading-4 text-center mb-4">
+                         {item.mainDiscount?.subtitle}
+                       </p>
+                       <span className="mb-6 text-sm text-gray700 flex items-center gap-x-4">
+                         Starting Price:
+                         <span className="bg-white px-3 py-1.5 text-[16px] font-semibold leading-6 text-gray900  ">
+                           ${item.mainDiscount?.price} USD
+                         </span>
+                       </span>
 
-                        <div className="w-full">
-                          <ShopNowBtn fullWidth />
+                       <div className="w-full">
+                         <ShopNowBtn fullWidth />
+                       </div>
+                     </div> */}
                         </div>
-                      </div>
+                      ))}
                     </MenubarSubContent>
                   </MenubarSub>
                 );
